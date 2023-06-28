6bf93fc6bc8e114d44ff2850a1af6433f52e1551
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2023-06-27 23:03:03 -0600

    Se empiezxa a añadir funcionalidad de JS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//QuerySelector
+
+const heading = document.querySelector(".header__texto h2"); //retorna 0 o 1 elementos
+
+heading.textContent = "Nuevo heading";
+heading.classList.add("Nueva-clase");
+
+console.log(heading);
+
+//QuerySelectorAll
+
+// getElementById
