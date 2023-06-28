b0f3f19c9a9db2cbbad45f8781d2c16d707ffe0f
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2023-06-27 23:27:47 -0600

    Generación de un nuevo enlace

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 const heading = document.querySelector(".header__texto h2"); //retorna 0 o 1 elementos
 
-// heading.textContent = "Nuevo heading";
+// heading.textContent = "Nuevo heading"; //?Se modifica el texto
 // heading.classList.add("Nueva-clase");
 
 console.log(heading); //?Retorna el primer elemento
@@ -16,3 +16,18 @@ console.log(enlaces);
 
 const heading2 = document.getElementById("prueba");
 console.log(heading2);
+
+//------------------------------------------------------------------------------------------------------------//
+//Generar nuevo enlace
+const nuevoEnlace = document.createElement("A");
+//Add href
+nuevoEnlace.href = "https://www.google.com";
+//Add texto
+nuevoEnlace.textContent = "Nuevo enlace";
+//Add clase
+nuevoEnlace.classList.add("navegacion__enlace");
+//Add to document
+const navegacion = document.querySelector(".navegacion");
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
