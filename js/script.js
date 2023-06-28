2582ece85be9154f611378226ecbcbe44cfcf77f
Jhosbyn Daniel <jhosbyndaniel@gmail.com>
2023-06-27 23:15:30 -0600

    Pruebas con 3 tipos de selectores

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@
 
 const heading = document.querySelector(".header__texto h2"); //retorna 0 o 1 elementos
 
-heading.textContent = "Nuevo heading";
-heading.classList.add("Nueva-clase");
+// heading.textContent = "Nuevo heading";
+// heading.classList.add("Nueva-clase");
 
-console.log(heading);
+console.log(heading); //?Retorna el primer elemento
 
 //QuerySelectorAll
 
+const enlaces = document.querySelectorAll(".navegacion a");
+console.log(enlaces);
+
 // getElementById
+
+const heading2 = document.getElementById("prueba");
+console.log(heading2);
